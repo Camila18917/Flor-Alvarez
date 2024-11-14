421487c64f243300a13183df59dd7d4bc068cc75
Camila18917 <camilagomez18917@gmail.com>
2024-11-13 21:47:51 -0300

    Actualizar

--- letra.js
+++ letra.js
@@ -3,11 +3,11 @@
 function buscarLetra(event) {
     event.preventDefault();
     const nombreCancion = document.getElementById('cancionb').value;
-    fetch(`https://api.lyrics.ovh/v1/Flor Alvarez/${nombreCancion}`)
+    fetch(`https://api.lyrics.ovh/v1/Yami Safdie/${nombreCancion}`)
       .then(response => response.json())
       .then(data => {
         if(data.cod == 404){
-          document.getElementById('clima').innerHTML = `<h2>No se encontró cancion</h2>`;
+          document.getElementById('clima').innerHTML = `<h2>No se encontró la ciudad</h2>`;
           return;
         }
         document.getElementById('clima').innerHTML = data.lyrics;
